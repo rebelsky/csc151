836af4e0fc91a87e8d9c9d244b5bc96fec1563b8
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-08-28 21:15:01 -0500

    Add the automatically-generiated "outlines.mk" which shows dependencies for outlines.

---
+++ outlines/outlines.mk
@@ -0,0 +1,280 @@
+outline.01.md: summaries/intro-algorithms.md \
+        overviews/intro-algorithms.md \
+        bodies/intro-algorithms.md \
+        related/related.01.md \
+        admin/admin.01.md
+outline.02.md: summaries/intro-linux.md \
+        overviews/intro-linux.md \
+        bodies/intro-linux.md \
+        related/related.02.md \
+        admin/admin.02.md
+outline.03.md: summaries/scheme.md \
+        overviews/scheme.md \
+        bodies/scheme.md \
+        related/related.03.md \
+        admin/admin.03.md
+outline.04.md: summaries/numbers.md \
+        overviews/numbers.md \
+        bodies/numbers.md \
+        related/related.04.md \
+        admin/admin.04.md
+outline.05.md: summaries/drawings.md \
+        overviews/drawings.md \
+        bodies/drawings.md \
+        related/related.05.md \
+        admin/admin.05.md
+outline.06.md: summaries/procedures.md \
+        overviews/procedures.md \
+        bodies/procedures.md \
+        related/related.06.md \
+        admin/admin.06.md
+outline.07.md: summaries/procedures-lab.md \
+        overviews/procedures-lab.md \
+        bodies/procedures-lab.md \
+        related/related.07.md \
+        admin/admin.07.md
+outline.08.md: summaries/intro-gimp.md \
+        overviews/intro-gimp.md \
+        bodies/intro-gimp.md \
+        related/related.08.md \
+        admin/admin.08.md
+outline.09.md: summaries/gimp-tools.md \
+        overviews/gimp-tools.md \
+        bodies/gimp-tools.md \
+        related/related.09.md \
+        admin/admin.09.md
+outline.10.md: summaries/design-colors.md \
+        overviews/design-colors.md \
+        bodies/design-colors.md \
+        related/related.10.md \
+        admin/admin.10.md
+outline.11.md: summaries/raster.md \
+        overviews/raster.md \
+        bodies/raster.md \
+        related/related.11.md \
+        admin/admin.11.md
+outline.12.md: summaries/colortrans.md \
+        overviews/colortrans.md \
+        bodies/colortrans.md \
+        related/related.12.md \
+        admin/admin.12.md
+outline.13.md: summaries/imagetrans.md \
+        overviews/imagetrans.md \
+        bodies/imagetrans.md \
+        related/related.13.md \
+        admin/admin.13.md
+outline.14.md: summaries/documentation.md \
+        overviews/documentation.md \
+        bodies/documentation.md \
+        related/related.14.md \
+        admin/admin.14.md
+outline.15.md: summaries/homogeneous-lists.md \
+        overviews/homogeneous-lists.md \
+        bodies/homogeneous-lists.md \
+        related/related.15.md \
+        admin/admin.15.md
+outline.16.md: summaries/anonymous-procedures.md \
+        overviews/anonymous-procedures.md \
+        bodies/anonymous-procedures.md \
+        related/related.16.md \
+        admin/admin.16.md
+outline.17.md: summaries/turtle-graphics.md \
+        overviews/turtle-graphics.md \
+        bodies/turtle-graphics.md \
+        related/related.17.md \
+        admin/admin.17.md
+outline.18.md: summaries/exam1.md \
+        overviews/exam1.md \
+        bodies/exam1.md \
+        related/related.18.md \
+        admin/admin.18.md
+outline.19.md: summaries/iteration.md \
+        overviews/iteration.md \
+        bodies/iteration.md \
+        related/related.19.md \
+        admin/admin.19.md
+outline.20.md: summaries/local.md \
+        overviews/local.md \
+        bodies/local.md \
+        related/related.20.md \
+        admin/admin.20.md
+outline.21.md: summaries/boolean.md \
+        overviews/boolean.md \
+        bodies/boolean.md \
+        related/related.21.md \
+        admin/admin.21.md
+outline.22.md: summaries/conditionals.md \
+        overviews/conditionals.md \
+        bodies/conditionals.md \
+        related/related.22.md \
+        admin/admin.22.md
+outline.23.md: summaries/testing.md \
+        overviews/testing.md \
+        bodies/testing.md \
+        related/related.23.md \
+        admin/admin.23.md
+outline.24.md: summaries/lists.md \
+        overviews/lists.md \
+        bodies/lists.md \
+        related/related.24.md \
+        admin/admin.24.md
+outline.25.md: summaries/recursion-basics.md \
+        overviews/recursion-basics.md \
+        bodies/recursion-basics.md \
+        related/related.25.md \
+        admin/admin.25.md
+outline.26.md: summaries/pause-02.md \
+        overviews/pause-02.md \
+        bodies/pause-02.md \
+        related/related.26.md \
+        admin/admin.26.md
+outline.27.md: summaries/helper-recursion.md \
+        overviews/helper-recursion.md \
+        bodies/helper-recursion.md \
+        related/related.27.md \
+        admin/admin.27.md
+outline.28.md: summaries/more-list-recursion.md \
+        overviews/more-list-recursion.md \
+        bodies/more-list-recursion.md \
+        related/related.28.md \
+        admin/admin.28.md
+outline.29.md: summaries/prepost.md \
+        overviews/prepost.md \
+        bodies/prepost.md \
+        related/related.29.md \
+        admin/admin.29.md
+outline.30.md: summaries/iterate-positions.md \
+        overviews/iterate-positions.md \
+        bodies/iterate-positions.md \
+        related/related.30.md \
+        admin/admin.30.md
+outline.31.md: summaries/local-procs.md \
+        overviews/local-procs.md \
+        bodies/local-procs.md \
+        related/related.31.md \
+        admin/admin.31.md
+outline.32.md: summaries/numeric-recursion.md \
+        overviews/numeric-recursion.md \
+        bodies/numeric-recursion.md \
+        related/related.32.md \
+        admin/admin.32.md
+outline.33.md: summaries/geometric-art.md \
+        overviews/geometric-art.md \
+        bodies/geometric-art.md \
+        related/related.33.md \
+        admin/admin.33.md
+outline.34.md: summaries/pairs.md \
+        overviews/pairs.md \
+        bodies/pairs.md \
+        related/related.34.md \
+        admin/admin.34.md
+outline.35.md: summaries/trees.md \
+        overviews/trees.md \
+        bodies/trees.md \
+        related/related.35.md \
+        admin/admin.35.md
+outline.36.md: summaries/vectors.md \
+        overviews/vectors.md \
+        bodies/vectors.md \
+        related/related.36.md \
+        admin/admin.36.md
+outline.37.md: summaries/randomness.md \
+        overviews/randomness.md \
+        bodies/randomness.md \
+        related/related.37.md \
+        admin/admin.37.md
+outline.38.md: summaries/strings.md \
+        overviews/strings.md \
+        bodies/strings.md \
+        related/related.38.md \
+        admin/admin.38.md
+outline.39.md: summaries/design.md \
+        overviews/design.md \
+        bodies/design.md \
+        related/related.39.md \
+        admin/admin.39.md
+outline.40.md: summaries/project-ideas.md \
+        overviews/project-ideas.md \
+        bodies/project-ideas.md \
+        related/related.40.md \
+        admin/admin.40.md
+outline.41.md: summaries/analyzing-procedures.md \
+        overviews/analyzing-procedures.md \
+        bodies/analyzing-procedures.md \
+        related/related.41.md \
+        admin/admin.41.md
+outline.42.md: summaries/association-lists.md \
+        overviews/association-lists.md \
+        bodies/association-lists.md \
+        related/related.42.md \
+        admin/admin.42.md
+outline.43.md: summaries/hop.md \
+        overviews/hop.md \
+        bodies/hop.md \
+        related/related.43.md \
+        admin/admin.43.md
+outline.44.md: summaries/binary-search.md \
+        overviews/binary-search.md \
+        bodies/binary-search.md \
+        related/related.44.md \
+        admin/admin.44.md
+outline.45.md: summaries/projects-0.md \
+        overviews/projects-0.md \
+        bodies/projects-0.md \
+        related/related.45.md \
+        admin/admin.45.md
+outline.46.md: summaries/sorting.md \
+        overviews/sorting.md \
+        bodies/sorting.md \
+        related/related.46.md \
+        admin/admin.46.md
+outline.47.md: summaries/insertion-sort.md \
+        overviews/insertion-sort.md \
+        bodies/insertion-sort.md \
+        related/related.47.md \
+        admin/admin.47.md
+outline.48.md: summaries/merge-sort.md \
+        overviews/merge-sort.md \
+        bodies/merge-sort.md \
+        related/related.48.md \
+        admin/admin.48.md
+outline.49.md: summaries/project-1.md \
+        overviews/project-1.md \
+        bodies/project-1.md \
+        related/related.49.md \
+        admin/admin.49.md
+outline.50.md: summaries/project-2.md \
+        overviews/project-2.md \
+        bodies/project-2.md \
+        related/related.50.md \
+        admin/admin.50.md
+outline.51.md: summaries/files.md \
+        overviews/files.md \
+        bodies/files.md \
+        related/related.51.md \
+        admin/admin.51.md
+outline.52.md: summaries/objects.md \
+        overviews/objects.md \
+        bodies/objects.md \
+        related/related.52.md \
+        admin/admin.52.md
+outline.53.md: summaries/pause-13.md \
+        overviews/pause-13.md \
+        bodies/pause-13.md \
+        related/related.53.md \
+        admin/admin.53.md
+outline.54.md: summaries/what-is-cs.md \
+        overviews/what-is-cs.md \
+        bodies/what-is-cs.md \
+        related/related.54.md \
+        admin/admin.54.md
+outline.55.md: summaries/wrapup.md \
+        overviews/wrapup.md \
+        bodies/wrapup.md \
+        related/related.55.md \
+        admin/admin.55.md
+outline.56.md: summaries/exam3.md \
+        overviews/exam3.md \
+        bodies/exam3.md \
+        related/related.56.md \
+        admin/admin.56.md
