a26c034df4195b5a125f774c5d127fb7891bb4be
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-01-10 14:56:37 -0600

    Changed form for notes (unused outlines).

--- outlines/notes.mk
+++ outlines/notes.mk
@@ -1,6 +1,6 @@
 # Instructions for creating the notes (pseudo-outlines) on topics that
 # are no longer covered.
 
-notes: spot-lists.md
-spot-lists.md: */spot-lists.md
+notes: notes.spot-lists.md
+notes.spot-lists.md: */spot-lists.md
 	../tools/mdnotes spot-lists "Spot Lists" > $@
